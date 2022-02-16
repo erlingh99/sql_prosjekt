8846d7a91b30de1ea4b54329d331ab5c69b9ed5d
samkaj <kajavasamuel@gmail.com>
2022-02-16 17:16:09 +0100

    Finalize registerStudent() trigger

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,5 @@
 -- Triggers
-CREATE FUNCTION registerStudent() RETURNS TRIGGER AS $registerStudent$
+CREATE OR REPLACE FUNCTION registerStudent() RETURNS TRIGGER AS $registerStudent$
     DECLARE
         missing         TEXT;
         fullCourse      BOOLEAN;
@@ -12,6 +12,11 @@ BEGIN
         RAISE EXCEPTION 'Student % is already registered in course %.', NEW.student, NEW.course;
     END IF;
 
+    -- Check if student is on waiting list for course
+    IF EXISTS (SELECT 1 FROM WaitingList WHERE WaitingList.student=NEW.student AND WaitingList.course=NEW.course) THEN
+        RAISE EXCEPTION 'Student % is already on waitinglist for course %.', NEW.student, NEW.course;
+    END IF; 
+
     -- Check if the student has completed the prerequired courses
     WITH
         requiredCourses AS
@@ -20,7 +25,7 @@ BEGIN
             (SELECT course FROM PassedCourses WHERE PassedCourses.student = NEW.student)        
     SELECT required INTO missing FROM requiredCourses WHERE NOT EXISTS (SELECT FROM passed WHERE course = required);
     
-    IF EXISTS missing THEN
+    IF missing IS NOT NULL THEN
         RAISE EXCEPTION 'All required courses are not passed. Missing courses: %', missing;
     END IF;
     
@@ -35,28 +40,26 @@ BEGIN
         END IF;
     END IF;
     
-    IF fullCourse THEN    
+    IF fullCourse THEN   
         INSERT INTO WaitingList VALUES (NEW.student, NEW.course);
         RAISE NOTICE 'Put on waitinglist for course %.', NEW.course;
     ELSE
         INSERT INTO Registered VALUES (NEW.student, NEW.course);
         RAISE NOTICE 'Registered for course %.', NEW.course;
     END IF;
+
+    RETURN NEW;
 END;
 $registerStudent$ LANGUAGE plpgsql;
 
-/*
-Registered(student, course)
-*/
-CREATE FUNCTION unregisterStudent() RETURNS TRIGGER 
-AS $unregisterStudent$
-DECLARE
-    studentExists   BOOLEAN;
-    courseExists    BOOLEAN;
-    isWaiting       BOOLEAN;
-    isLimited       BOOLEAN;
-    isRegistered    BOOLEAN;
-    newStudent      CHAR(10);
+CREATE FUNCTION unregisterStudent() RETURNS TRIGGER AS $unregisterStudent$
+    DECLARE
+        studentExists   BOOLEAN;
+        courseExists    BOOLEAN;
+        isWaiting       BOOLEAN;
+        isLimited       BOOLEAN;
+        isRegistered    BOOLEAN;
+        newStudent      CHAR(10);
 BEGIN
     studentExists := EXISTS (SELECT 1 FROM Students WHERE OLD.student = studentIdnr);
     courseExists := EXISTS (SELECT 1 FROM Courses WHERE OLD.course = code);
@@ -96,28 +99,13 @@ BEGIN
         INSERT INTO Registered VALUES(newStudent, OLD.course);
         DELETE FROM WaitingList WHERE (WaitingList.student = newStudent AND WaitingList.Course = OLD.course);
     END IF;
+
+    RETURN NULL;
 END;
 $unregisterStudent$ LANGUAGE plpgsql;
-
-
 
 CREATE TRIGGER unregisterStudent INSTEAD OF DELETE ON Registrations 
             FOR EACH ROW EXECUTE FUNCTION unregisterStudent();
 
-
 CREATE TRIGGER registerStudent INSTEAD OF INSERT ON Registrations 
-            FOR EACH STATEMENT EXECUTE FUNCTION registerStudent();
-
-
-/*
-ERROR:  cannot insert into view "registrations"
-DETAIL:  Views containing UNION, INTERSECT, or EXCEPT are not automatically updatable.
-HINT:  To enable inserting into the view, provide an INSTEAD OF INSERT trigger or an unconditional ON INSERT DO INSTEAD rule.
-
-
-ERROR:  trigger functions cannot have declared arguments
-HINT:  The arguments of the trigger can be accessed through TG_NARGS and TG_ARGV instead.
-CONTEXT:  compilation of PL/pgSQL function "unregisterstudent" near line 1
-*/
-
-SELECT EXISTS(SELECT 'CCC222' FROM LimitedCourses WHERE LimitedCourses.code = 'CCC222')
+            FOR EACH ROW EXECUTE FUNCTION registerStudent();
