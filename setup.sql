2cea502a615f3f8192fb684f476cf90ee08e8e72
samkaj <kajavasamuel@gmail.com>
2022-02-16 17:15:14 +0100

    Setup: add TODO

--- setup.sql
+++ setup.sql
@@ -1,1 +1,9 @@
 -- Setup
+/*
+TODO:
+SQL code that sets up your database for testing the triggers. 
+This will normally be the concatenation of files tables.sql, insert.sql and views.sql 
+from part 2. 
+We must be able to test your triggers by executing the files setup.sql and triggers.sql,
+in that order.
+*/
