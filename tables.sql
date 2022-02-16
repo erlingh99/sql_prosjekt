74ed640ea9bcbcfc3b469fd199af8e54fc466780
Erling Hjermstad <erling-hjermstad@hotmail.com>
2022-02-16 12:49:40 +0100

    database implementation

---
+++ tables.sql
@@ -0,0 +1,131 @@
+-- Tables
+CREATE TABLE Departments(
+    name         TEXT PRIMARY KEY,
+    abbriviation TEXT UNIQUE
+);
+
+CREATE TABLE Programs(
+    name         TEXT PRIMARY KEY,
+    abbriviation TEXT
+);
+
+--what departments hold what programs, assume many to many relationship
+CREATE TABLE DepartmentPrograms( 
+    departmentName TEXT REFERENCES Departments,
+    programName    TEXT REFERENCES Programs
+);
+
+CREATE TABLE Branches(
+    name        TEXT,
+    program     TEXT REFERENCES Programs,
+
+    PRIMARY KEY (name, program)
+);
+
+CREATE TABLE Students(
+    idnr        CHAR(10) PRIMARY KEY CHECK (idnr SIMILAR TO '[0-9]{10}'),
+    name        TEXT NOT NULL,
+    login       TEXT NOT NULL UNIQUE,
+    program     TEXT NOT NULL REFERENCES Programs,
+
+    UNIQUE (idnr, program)
+);
+
+CREATE TABLE Courses(
+    code        CHAR(6) PRIMARY KEY,
+    name        TEXT NOT NULL,
+    credits     FLOAT NOT NULL CHECK (credits >= 0), --got feedback to change allow 0 credits, but this was already allowed. No changes made
+    department  TEXT REFERENCES Departments
+);
+
+CREATE TABLE Prerequisites(
+    course      CHAR(6) REFERENCES Courses,
+    required    CHAR(6) REFERENCES Courses,
+
+    CHECK (course != required),
+    PRIMARY KEY (course, required)
+);
+
+-- -- From BCNF. Decomposed into courses, limitedcourses and classified
+-- CREATE TABLE R2(
+--     courseCode      CHAR(6) PRIMARY KEY REFERENCES Courses,
+--     courseName      TEXT NOT NULL,
+--     credits         FLOAT NOT NULL CHECK (credits >= 0),
+--     departmentName  TEXT REFERENCES Departments,
+--     capacity        INT CHECK (capacity > 0),
+--     classification  TEXT REFERENCES Classifications
+-- );  
+
+CREATE TABLE LimitedCourses(
+    code        CHAR(6) PRIMARY KEY REFERENCES Courses,
+    capacity    INTEGER NOT NULL CHECK (capacity > 0)
+);
+
+CREATE TABLE MandatoryProgram(
+    course      CHAR(6) REFERENCES Courses,
+    program     TEXT REFERENCES Programs,
+
+    PRIMARY KEY (course, program)
+);
+
+CREATE TABLE MandatoryBranch(
+    course      CHAR(6) REFERENCES Courses,
+    branch      TEXT,
+    program     TEXT,
+
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (branch, program) REFERENCES Branches
+);
+
+CREATE TABLE RecommendedBranch(
+    course      CHAR(6) REFERENCES Courses,
+    branch      TEXT,
+    program     TEXT,
+
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (branch, program) REFERENCES Branches
+);
+
+CREATE TABLE Taken(
+    student     CHAR(10) REFERENCES Students,
+    course      CHAR(6) REFERENCES Courses,
+    grade       CHAR(1) NOT NULL CHECK (grade IN ('U', '3', '4', '5')),
+
+    PRIMARY KEY (student, course) --shouldn't a student be able to retake a course?
+);
+
+CREATE TABLE StudentBranches(
+    student     CHAR(10) PRIMARY KEY REFERENCES Students,
+    branch      TEXT NOT NULL,
+    program     TEXT NOT NULL,
+    
+    FOREIGN KEY (student, program) REFERENCES Students(idnr, program),
+    FOREIGN KEY (branch, program) REFERENCES Branches
+);
+
+CREATE TABLE Classifications(
+    name        TEXT PRIMARY KEY 
+);
+
+CREATE TABLE Classified(
+    course          CHAR(6) REFERENCES Courses,
+    classification  TEXT REFERENCES Classifications,
+
+    PRIMARY KEY (course, classification)
+);
+
+CREATE TABLE Registered(
+    student     CHAR(10) REFERENCES Students,
+    course      CHAR(6) REFERENCES Courses,
+
+    PRIMARY KEY (student, course)
+);
+
+CREATE TABLE WaitingList(
+    student     CHAR(10) REFERENCES Students,
+    course      CHAR(6) REFERENCES LimitedCourses,
+    --position    TIMESTAMP NOT NULL DEFAULT NOW(),
+    position    SERIAL UNIQUE, --SERIAL is guaranteed not null and concurrency safe
+
+    PRIMARY KEY (student, course)
+);
---
+++ views.sql
@@ -0,0 +1,71 @@
+-- Views
+CREATE VIEW BasicInformation AS (
+    SELECT idnr, name, login, Students.program, branch 
+    FROM Students LEFT JOIN StudentBranches ON idnr = student
+);
+
+CREATE VIEW FinishedCourses AS (
+    SELECT student, course, grade, Courses.credits
+    FROM Taken JOIN Courses
+    ON Taken.course = Courses.code    
+);
+
+CREATE VIEW PassedCourses AS (
+    SELECT student, course, credits FROM FinishedCourses
+    WHERE grade != 'U'
+);
+
+CREATE VIEW Registrations AS (
+    SELECT student, course, 'waiting' AS status FROM WaitingList
+    UNION
+    SELECT student, course, 'registered' AS status FROM Registered
+);
+
+--students who haven't chosen a branch won't appear here, but cannot collect recommendedcredits and therefore not graduate
+CREATE VIEW UnreadMandatory AS (
+    SELECT idnr AS student, MandatoryProgram.course
+    FROM Taken RIGHT JOIN
+    (Students NATURAL JOIN Mandatoryprogram)
+    ON (taken.student, taken.course) = (students.idnr, mandatoryprogram.course)
+    WHERE grade IS NULL OR grade = 'U'
+    UNION
+    SELECT StudentBranches.student, MandatoryBranch.course
+    FROM Taken RIGHT JOIN
+    (StudentBranches NATURAL JOIN MandatoryBranch)
+    ON (taken.student, taken.course) = (StudentBranches.student, MandatoryBranch.course)
+    WHERE grade IS NULL OR grade = 'U'
+);
+
+-- can also be included in the with-statement of path to graduation instead of being a view
+CREATE VIEW PassedClassifications AS (
+    SELECT * FROM PassedCourses NATURAL JOIN Classified
+);
+
+CREATE VIEW PathToGraduation AS (
+    WITH     
+        student AS
+            (SELECT idnr AS student FROM BasicInformation),
+        totalCredits AS
+            (SELECT student.student, COALESCE(SUM(credits), 0) AS totalCredits FROM student NATURAL LEFT JOIN PassedCourses GROUP BY student),
+        mandatoryLeft AS
+            (SELECT student.student, COALESCE(COUNT(course), 0) AS mandatoryLeft FROM student NATURAL LEFT JOIN UnreadMandatory GROUP BY student),
+        seminarCourses AS
+            (SELECT student, COUNT(course) AS seminarCourses FROM PassedClassifications WHERE classification = 'seminar' GROUP BY student),
+        researchCredits AS
+            (SELECT student, SUM(credits) AS researchCredits FROM PassedClassifications WHERE classification = 'research' GROUP BY student),
+        mathCredits AS
+            (SELECT student, SUM(credits) AS mathCredits FROM PassedClassifications WHERE classification = 'math' GROUP BY student),
+        recommendedCredits AS
+            (SELECT student.student, COALESCE(SUM(credits), 0) AS recommendedCredits
+            FROM student NATURAL LEFT JOIN (PassedCourses NATURAL JOIN (StudentBranches NATURAL JOIN RecommendedBranch))
+            GROUP BY student.student)
+            
+    SELECT student, totalCredits, mandatoryLeft, COALESCE(mathCredits, 0) AS mathCredits, COALESCE(researchCredits, 0) AS researchCredits, COALESCE(seminarCourses, 0) AS seminarCourses,
+            COALESCE(mandatoryLeft = 0 AND recommendedCredits >= 10 AND mathCredits >= 20 AND researchCredits >= 10 AND seminarCourses >= 1, false) AS qualified
+
+    FROM student NATURAL LEFT JOIN totalCredits NATURAL LEFT JOIN mandatoryLeft NATURAL LEFT JOIN mathCredits NATURAL LEFT JOIN researchCredits NATURAL LEFT JOIN seminarCourses NATURAL LEFT JOIN recommendedCredits
+);
+
+CREATE VIEW WaitinglistSorted AS (
+    SELECT * FROM WaitingList ORDER BY course ASC, position ASC
+);
